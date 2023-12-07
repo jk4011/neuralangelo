74eb84e040e1656704ec0afb101b7d9f7c916c7f
Jinhyeok <jinhyeok@unist.ac.kr>
2023-12-07 11:48:33 +0900

    bash script for training

--- bash/dtu_train.bash
+++ bash/dtu_train.bash
@@ -1,12 +1,15 @@
+GPUS=0,3,4,5,6,7
+NGPUS=6  # use >1 for multi-GPU training!
+
 EXPERIMENT=dtu
 GROUP=example_group
 CONFIG=projects/neuralangelo/configs/${EXPERIMENT}.yaml
-GPUS=1  # use >1 for multi-GPU training!
 
-for IDX in "40" "55" "63" "65" "69" "83" "97" "105" "106" "110" "114" "118" "122"; do
+for IDX in "24" "37" "40" "55" "63" "65" "69" "83" "97" "105" "106" "110" "114" "118" "122"; do
+    echo 1111
     NAME="dtu$IDX"
     sed -i "s/dtu_scan[0-9]\+/dtu_scan$IDX/g" projects/neuralangelo/configs/dtu.yaml
-    torchrun --nproc_per_node=${GPUS} train.py \
+    CUDA_VISIBLE_DEVICES=${GPUS} torchrun --nproc_per_node=${NGPUS} train.py \
         --logdir=logs/${GROUP}/${NAME} \
         --config=${CONFIG} \
         --show_pbar \
---
+++ move_data.bash
@@ -0,0 +1,7 @@
+# cp /data/wlsgur4011/DataCollection/*.zip /data2/wlsgur4011/data/
+cd /data2/wlsgur4011/data/
+unzip \*.zip
+cd /data2/wlsgur4011/neuralangelo/
+mkdir datasets && cd datasets
+ln -s /data2/wlsgur4011/data/dtu_neus dtu
+cd ../..
