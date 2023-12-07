d26ef4398fc9b1975b415db83bf8dba76eb50388
Jinhyeok <jinhyeok@unist.ac.kr>
2023-12-07 20:34:29 +0900

    bash script for data mv

--- bash/move_dtu.bash
+++ bash/move_dtu.bash
@@ -2,10 +2,13 @@
 set -o xtrace # print command
 
 mkdir /data2/wlsgur4011/data/
-cp /data/wlsgur4011/DataCollection/*.zip /data2/wlsgur4011/data/
+cp /data/wlsgur4011/DataCollection/dtu_neus.zip /data2/wlsgur4011/data/
+
 cd /data2/wlsgur4011/data/
-unzip \*.zip
+unzip dtu_neus.zip
+mv data tanks_and_temples
 cd /data2/wlsgur4011/neuralangelo/
-mkdir datasets && cd datasets
+
+mkdir datasets
+cd datasets
 ln -s /data2/wlsgur4011/data/dtu_neus dtu
-cd ../..
