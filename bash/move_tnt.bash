77a74acc1b37a3fdea83c86370b8d586bc582619
Jinhyeok <jinhyeok@unist.ac.kr>
2023-12-08 18:48:25 +0900

    .

--- bash/move_tnt.bash
+++ bash/move_tnt.bash
@@ -2,13 +2,11 @@
 set -o xtrace # print command
 
 mkdir /data2/wlsgur4011/data/
-cp /data/wlsgur4011/DataCollection/tanks_and_temples/tanks_and_temples.zip /data2/wlsgur4011/data/
-
+cp /data/wlsgur4011/DataCollection/tanks_and_temples.zip /data2/wlsgur4011/data/
 cd /data2/wlsgur4011/data/
 unzip tanks_and_temples.zip
-mv data tanks_and_temples
-cd /data2/wlsgur4011/neuralangelo/
 
+cd /data2/wlsgur4011/neuralangelo/
 mkdir datasets
 cd datasets
 ln -s /data2/wlsgur4011/data/tanks_and_temples/ tanks_and_temples
