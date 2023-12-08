0c2ad2a7c5a6c579abf0fa980a2241a72b6fa741
Jinhyeok <jinhyeok@unist.ac.kr>
2023-12-08 19:52:01 +0900

    bash script for tnt train

---
+++ bash/resume_single_dtu_train.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+START_IDX=$1
+GPUS=(${@:2})
+NGPUS=${#GPUS[@]}
+
+EXPERIMENT=dtu
+GROUP=example_group
+CONFIG=projects/neuralangelo/configs/${EXPERIMENT}.yaml
+
+IDX_LIST=("24" "37" "40" "55" "63" "65" "69" "83" "97" "105" "106" "110" "114" "118" "122")
+IDX_LEN=${#IDX_LIST[@]}
+
+for (( j=0; j<${NGPUS}; j++ )); do
+    if (( i+j < IDX_LEN )); then
+        SMALL_IDX=$((START_IDX+i+j))
+        IDX=${IDX_LIST[SMALL_IDX]}
+        NAME="dtu$IDX($SMALL_IDX)"
+
+        # if IDX is larger than len IDX_LEN, break
+        if [ $SMALL_IDX -ge $IDX_LEN ]; then
+            echo "Index out of range, breaking the loop"
+            break
+        fi
+
+        echo 1111
+        echo $NAME
+        
+        CKPT_PATH=`ls logs/${GROUP}/${NAME}/*.pt | tail -n 1`
+        sed -i "s/dtu_scan[0-9]\+/dtu_scan$IDX/g" projects/neuralangelo/configs/dtu.yaml
+        PORT=$((26000 + $j))
+        CUDA_VISIBLE_DEVICES=${GPUS[j]} torchrun --master_port=${PORT} train.py \
+            --logdir=logs/${GROUP}/${NAME} \
+            --config=${CONFIG} \
+            --show_pbar \
+            --resume \
+            --checkpoint=${CKPT_PATH} \
+            --wandb &
+        #     # 2>&1 > logs/${GROUP}/commmand_log 
+        sleep 10
+    fi
+done
---
+++ bash/single_tnt_train.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+START_IDX=$1
+GPUS=(${@:2})
+NGPUS=${#GPUS[@]}
+
+EXPERIMENT=tnt
+GROUP=example_group
+CONFIG=projects/neuralangelo/configs/${EXPERIMENT}.yaml
+
+DATA_LIST=(Barn Ignatius Truck Caterpillar Courthouse Meetingroom)
+DATA_LEN=${#DATA_LIST[@]}
+
+for (( j=0; j<${NGPUS}; j++ )); do
+    if (( i+j < DATA_LEN )); then
+        IDX=$((START_IDX+i+j))
+        DATA=${DATA_LIST[IDX]}
+        NAME="${IDX}.${DATA}"
+
+        # if IDX is larger than len DATA_LEN, break
+        if [ $IDX -ge $DATA_LEN ]; then
+            echo "Index out of range, breaking the loop"
+            break
+        fi
+
+        echo 1111
+        echo $NAME
+        N_IMAGES=`ls datasets/tanks_and_temples/${DATA}/images | wc -l`
+        sed -i "s/root\:\ datasets\/tanks_and_temples\/\w*/root\:\ datasets\/tanks_and_temples\/${DATA}/g"  projects/neuralangelo/configs/tnt.yaml
+        sed -i "s/num_images\:\ [0-9]*/num_images: ${N_IMAGES}/g" projects/neuralangelo/configs/tnt.yaml
+        PORT=$((27777 + $j))
+        CUDA_VISIBLE_DEVICES=${GPUS[j]} torchrun --master_port=${PORT} train.py \
+            --logdir=logs/${GROUP}/${NAME} \
+            --config=${CONFIG} \
+            --show_pbar \
+            --wandb &
+            # 2>&1 > logs/${GROUP}/commmand_log 
+        sleep 10
+    fi
+done
